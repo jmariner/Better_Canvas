8c2d1c1b4bfaab19922d8568475c054863e9b7af
Justin Mariner <justin.mariner@hotmail.com>
2018-07-10 11:01:14 -0400

    Updated icon pulling script for Instructure-UI
    Instructure moved their icons to instructure/instructure-ui under the
    scope of @instructure on NPM. The new script pulls the SCSS map file
    from unpkg instead of rawgit.

--- scripts/update-icons.js
+++ scripts/update-icons.js
@@ -1,10 +1,24 @@
 const download = require("download");
 const { resolve } = require("path");
+const fs = require("fs");
 
-const destination = resolve(__dirname, "../src/scss/")
+const filename = "_canvas_icons.scss";
+const destination = resolve(__dirname, "../src/scss/", filename);
 
-const blob = process.argv[2] || "gh-pages";
-const url = "https://raw.githubusercontent.com/instructure/instructure-icons/" +
-			blob + "/fonts/Solid/InstructureIcons-Solid_icon-map.scss"
+const version = process.argv[2] || "latest";
+const package = "@instructure/ui-icons";
+const filePath = "lib/font/Solid/InstructureIcons-Solid_icon-map.scss";
+const url = `https://unpkg.com/${package}@${version}/${filePath}`;
 
-download(url, destination, { filename: "_canvas_icons.scss" });
+// download the SCSS map file given the above URL.
+// store the resulting version in a comment in the saved SCSS file.
+download(url, {stream: true}).on("response", resp => {
+
+	const [, respVersion] = resp.url.match(new RegExp(package + "@([\\d.]+)"));
+	const preface = `/* VERSION: ${respVersion}${version === "latest" ? " (latest)" : ""} */\n`
+	let scss = "";
+
+	resp.on("data", data => { scss += data.toString(); });
+	resp.on("end", () => { fs.writeFileSync(destination, preface + scss); });
+
+});
